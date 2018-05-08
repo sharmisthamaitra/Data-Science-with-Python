a3b661838c33daf964424c42d856bb5de97c1e40
Sharmistha <sharmisthamaitra@yahoo.com>
2018-05-08 13:17:45 -0700

    initial comment in git by sharmistha

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Data science project for Titanic dataset in python using cookiecutter template and jupyter notebook',
+    author='Sharmistha Maitra',
+    license='MIT',
+)
